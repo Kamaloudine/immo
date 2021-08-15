f294036590f72dab2d5184024ee1f5b5330f05e7
kamaloudine <kamaloudine2017@gmail>
2021-08-15 02:39:18 +0000

    API AUTH OK

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', [AuthController::class, 'authenticate']);
+Route::post('register', [AuthController::class, 'register']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('get_user', [AuthController::class, 'get_user']);
+    /* Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{id}', [ProductController::class, 'show']);
+    Route::post('create', [ProductController::class, 'store']);
+    Route::put('update/{product}',  [ProductController::class, 'update']);
+    Route::delete('delete/{product}',  [ProductController::class, 'destroy']); */
 });
